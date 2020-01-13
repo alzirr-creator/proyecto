db4de65361d403f8c0346b134db2e37d7e06ab08
Jorge Carpena <jorge.carpena@tecsup.edu.pe>
2020-01-13 10:06:09 -0500

    gaaaaaaaaaaaaa

---
+++ Proyecto-DI2-master/js/sistema-de-tema.js
@@ -0,0 +1,28 @@
+var tema = document.getElementById('tema');
+var cuerpo2 = document.getElementById('info1');
+var cuerpo3 = document.getElementById('info2');
+var cuerpo4 = document.getElementById('redes-sociales');
+var cuerpo5 = document.getElementById('body');
+var cuerpo6 = document.getElementById('frase');
+var cuerpo7 = document.getElementById('testimonio1')
+var cuerpo8 = document.getElementById('testimonio3')
+var cuerpo9 = document.getElementById('asd')
+var cuerpo10 = document.getElementById('formbutton')
+var cuerpo11 = document.getElementById('marcas')
+
+$(tema).click(function() {
+ 
+ $( cuerpo2 ).toggleClass( "black" );
+ $( cuerpo3 ).toggleClass( "black" );
+ $( cuerpo4 ).toggleClass( "black" );
+ $( cuerpo5 ).toggleClass( "black" );
+ $( cuerpo6 ).toggleClass( "black3" );
+ $( cuerpo7 ).toggleClass( "black3" );
+ $( cuerpo8 ).toggleClass( "black3" );
+ $( cuerpo9 ).toggleClass( "black4" );
+ $( cuerpo10 ).toggleClass( "black5" );
+ $( cuerpo11 ).toggleClass( "black3" );
+
+});
+
+
